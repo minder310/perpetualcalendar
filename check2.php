4087e420b19c79fb79739f5014d73dd73bb3305d
Pierre.K <minder309@gmail.com>
2022-11-04 08:35:46 +0800

    new file:   check2.php 新增確認網站

---
+++ check2.php
@@ -0,0 +1,52 @@
+<?php
+// 宣告使用session資料。
+session_start();
+
+// 宣告正確的帳號密碼用以比對。
+$users=[
+    [
+        "name"=>"mack",
+        "pw"=>"1234",
+        "level"=>"admin",
+    ],
+    [
+        "name"=>"john",
+        "pw"=>"5678",
+        "level"=>"user",
+    ],
+    [
+        "name"=>"mary",
+        "pw"=>"1357",
+        "level"=>"vip",
+    ]
+];
+// 接收傳過來的帳號密碼。
+$formAcc=$_POST['acc'];
+$formPw=$_POST['pw'];
+
+// 一開始宣告$chk=false;
+$chk=false;
+foreach($users as $user){
+    if($user['name']==$formAcc && $user['pw']==$formPw){
+        $chk=true;
+        $_SESSION['login']=$user;
+    }
+}
+// 這句話是$chk為真。
+if($chk){
+    // 取用cookie內的time並且+1
+    $time=$_COOKIE['time']+1;
+    // 宣告cookie內的time陣列，內的time=$time，並且時間家1年。
+    setcookie('time',$time,time()+(60*60*24*365));
+}else{
+    $error="帳號密碼錯誤";
+}
+// 要是$error有值。
+if(isset($error)){
+    // 這邊的意思是導回頁面。但不太理解header
+    header("location:login2.php?error=$error");
+}else{
+    header("location:login2.php");
+}
+
+?>
